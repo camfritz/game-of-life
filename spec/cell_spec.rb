bd47ed1f5a2acfca9beb9b7231a76b869995da88
Camron Fritz <camfritz@gmail.com>
2018-06-11 14:50:59 -0700

    Add/fail unit test for a dead cell with exactly three live neighbors becomes alive

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,54 +1,49 @@
 require_relative '../lib/cell'
 
 describe "A cell" do
-  # before(:each) do
-  #   let(:cell) { Cell.new }
-  # end
+  let(:cell) {Cell.new}
 
   it 'exists' do
-    Cell.new
+
   end
 
   it 'is initially alive' do
-    cell = Cell.new
     expect(cell.alive?).to be_truthy
   end
 
   it 'can die' do
-    cell = Cell.new
     expect(cell.alive?).to be_truthy
     cell.kill
     expect(cell.alive?).to be_falsy
   end
 
   it 'has neighbors, which is initially an empty array' do
-    cell = Cell.new
     expect(cell.neighbors).to be_empty
   end
 
   it 'has a tick method' do
-    cell = Cell.new
     expect(cell).to respond_to(:tick)
   end
 
   describe 'after a tick' do
 
     it 'a live cell with fewer than two live neighbors dies' do
-      cell = Cell.new
       expect(cell.neighbors.count).to be_within(1).of(0)
       cell.tick
       expect(cell.alive?).to be_falsy
     end
 
     it 'a dead cell with fewer than two live neighbors stays dead' do
-      cell = Cell.new
       cell.kill
       expect(cell.neighbors.count).to be_within(1).of(0)
       expect(cell.alive?).to be_falsy
     end
 
     # TODO: Cell rules
-
+    it 'a dead cell with exactly three live neighbors becomes alive' do
+      expect(cell.neighbors.count).to eq(3)
+      expect(cell.alive?).to be_truthy
+    end
   end
 
 end
